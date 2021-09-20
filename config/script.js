15a55a6c8bdc8948b39fca552074a161cc96581b
davidson <davidsonmarra@gmail.com>
2021-09-19 22:38:32 -0300

    add config js

--- config/script.js
+++ config/script.js
@@ -1,5 +1,48 @@
 function myFunction() {
-    var x = document.getElementById("config-text").value;
-    document.getElementById("demo").innerHTML = x;
-    window.location.href = 'http://google.com/?i=' + x;
+    let data = [];
+    let aux = []
+    let formattedData = {
+        id: 0,
+        account: '',
+        role: '',
+        region: '',
+        color: ''
+    }
+    let x = document.getElementById("config-text").value;
+    // document.getElementById("demo").innerHTML = x;
+    let y = x.replaceAll('=', ':').replaceAll(']', '').split('[').filter(ln => ln !== '')
+    
+    y.map((ln, i) => {
+        if(ln[ln.length-1] === '\n')
+            ln[ln.length-1] = '';
+        ln = ln.replaceAll('\n', ',');
+        ln = `id:${ln}`;
+        ln = ln.split(',').filter(ln => ln !== '');
+        data[i] = ln;
+        data[i].map((dt, j) => {
+            dt = dt.split(':');
+            
+            switch (dt[0]) {
+                case 'id':
+                    formattedData.id = dt[1];
+                    break;
+                case 'aws_account_id':
+                    formattedData.account = dt[1];
+                    break;
+                case 'role_name':
+                    formattedData.role = dt[1];
+                    break;
+                case 'region':
+                    formattedData.region = dt[1];
+                    break;
+                case 'color':
+                    formattedData.color = dt[1];
+                    break;
+                default:
+                    break;
+            }
+        })
+        aux[i] = {...formattedData};
+    });
+    localStorage.setItem('@swap-role:data', JSON.stringify(aux));
 }
