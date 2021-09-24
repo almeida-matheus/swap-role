7a11f204e1148640dd16c1e26016d64244ab47a7
almeida-matheus <matheusalmeidacosta1212@gmail.com>
2021-09-23 22:52:41 -0300

    add dynamic list popup

---
+++ popup/script.js
@@ -0,0 +1,30 @@
+const storage_data = JSON.parse(localStorage.getItem('@swap-role:data'));
+
+function addElement(data) {
+    let id = data.account;
+    let account = data.id;
+    let color = data.color;
+    let region = data.region;
+    let role = data.role;
+
+    const a = document.createElement('a');
+    a.setAttribute('href', `https://signin.aws.amazon.com/switchrole?account=${id}&roleName=${role}&displayName=${account}&color=${color}`);
+
+    a.innerHTML = `
+        <div class="ball"></div>
+        <span class="color"></span>
+        <b>${account}</b><span class="id">${id}</span>
+    `;
+
+    document.getElementById('list').appendChild(a)
+}
+
+function main() {
+    if (storage_data) {
+        storage_data.forEach((data) => {
+            addElement(data);
+        });
+    }
+}
+
+main()
