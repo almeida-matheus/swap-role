957adf1129f94489c125bf1f4fa59ddc33a49660
almeida-matheus <matheusalmeidacosta1212@gmail.com>
2021-11-13 00:34:05 -0300

    add function to save text in textarea

--- config/script.js
+++ config/script.js
@@ -1,4 +1,4 @@
-function saveText() {
+document.getElementById("save").addEventListener("click", () => {
     let data = [];
     let aux = []
     let formattedData = {
@@ -8,20 +8,20 @@ function saveText() {
         region: '',
         color: ''
     }
-    let x = document.getElementById("config-text").value;
+    let textarea = document.getElementById("config-text").value;
 
-    let y = x.replaceAll('=', ':').replaceAll(']', '').split('[').filter(ln => ln !== '')
-    
+    let y = textarea.replaceAll('=', ':').replaceAll(']', '').split('[').filter(ln => ln !== '')
+
     y.map((ln, i) => {
-        if(ln[ln.length-1] === '\n')
-            ln[ln.length-1] = '';
+        if (ln[ln.length - 1] === '\n')
+            ln[ln.length - 1] = '';
         ln = ln.replaceAll('\n', ',');
         ln = `id:${ln}`;
         ln = ln.split(',').filter(ln => ln !== '');
         data[i] = ln;
         data[i].map((dt, j) => {
             dt = dt.split(':');
-            
+
             switch (dt[0]) {
                 case 'id':
                     formattedData.id = dt[1];
@@ -42,10 +42,29 @@ function saveText() {
                     break;
             }
         })
-        aux[i] = {...formattedData};
+        aux[i] = { ...formattedData };
     });
     localStorage.setItem('@swap-role:data', JSON.stringify(aux));
     // LocalStorageSet(aux)
-}
+})
 
-document.getElementById('save').addEventListener('click', saveText);
+document.addEventListener("DOMContentLoaded", () => {
+    let textarea = document.getElementById("config-text")
+    let str_swap = localStorage.getItem('@swap-role:data')
+    let all_text = ``
+    if (str_swap) {
+        const json_swap = JSON.parse(str_swap)
+
+        json_swap.forEach((profile) => {
+            let each_text_profile = `[${profile["id"]}]
+aws_account_id=${profile["account"]}
+role_name=${profile["role"]}
+region=${profile["region"]}
+color=${profile["color"]}
+
+`
+            all_text = all_text + each_text_profile;
+        });
+    }
+    textarea.value = all_text;
+})
