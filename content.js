e24181d1c67f35d670a97eae0f40660951bd8fc6
almeida-matheus <matheusalmeidacosta1212@gmail.com>
2022-01-23 12:47:56 -0300

    add function to filter roles in popup

--- content.js
+++ content.js
@@ -2,13 +2,16 @@ const config_value = LocalStorageGet('@swap-role:data')
 
 function checkButton(e) {
     if (window.location.href.indexOf('signin') > -1) {
-        let timer = setInterval(tryClick, 5);
-        let max_attempts = 2000;
+        // every 500 ms, run tryClick()
+        let timer = setInterval(tryClick, 500);
+        // stop trying after 20 attempts.
+        let max_attempts = 20;
         function tryClick() {
             max_attempts -= 1;
             if (max_attempts > 0) {
                 clearInterval(timer);
                 let button = document.getElementById('input_switchrole_button');
+                // if found the button click on it.
                 if (button) {
                     max_attempts = 0;
                     button.click();
--- popup/script.js
+++ popup/script.js
@@ -1,5 +1,6 @@
 const storage_data = JSON.parse(localStorage.getItem('@swap-role:data'));
 
+//! add each role inside a popup list
 function addElement(data) {
     let id = data.account;
     let account = data.id;
@@ -7,6 +8,7 @@ function addElement(data) {
     let region = data.region;
     let role = data.role;
 
+    const li = document.createElement('li');
     const a = document.createElement('a');
     a.setAttribute('href', `https://signin.aws.amazon.com/switchrole?account=${id}&roleName=${role}&displayName=${account}&color=${color}&region=${region}}`);
 
@@ -16,7 +18,8 @@ function addElement(data) {
         <b>${account}</b><span class="id">${id}</span>
     `;
 
-    document.getElementById('list').appendChild(a)
+    document.getElementById('list').appendChild(li)
+    li.prepend(a)
 }
 
 function main() {
@@ -29,6 +32,29 @@ function main() {
 
 main()
 
+//! filter values that are inside a list of roles
+document.getElementById("input-list").addEventListener('keyup', FilterList);
+function FilterList() {
+    console.log('ativado')
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('input-list');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("list");
+    li = ul.getElementsByTagName('li');
+
+    //! loop through all list items and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+//! enable function for link to open in the same tab
 var hrefs = document.getElementsByTagName("a");
 
 function openLink() {
